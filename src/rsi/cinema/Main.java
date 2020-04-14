b598aa7ac3fd5d693e37334cef0f9095c237dff9
mazsak <mazsak97@gmail.com>
2020-04-14 20:28:26 +0200

    init project

---
+++ src/rsi/cinema/Main.java
@@ -0,0 +1,10 @@
+package rsi.cinema;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i =0; i < 1000000; i++)
+            System.out.println("Adas pajacan");
+    }
+}
