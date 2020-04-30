c478e2f35d7db85bd8b4ebd360fd0d5069c3bff7
adam <adampolejczuk@gmail.com>
2020-04-30 22:27:57 +0200

    Add mail sending

--- src/main/java/web_servies/ReservationService.java
+++ src/main/java/web_servies/ReservationService.java
@@ -10,10 +10,11 @@ import entities.User;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @WebService(serviceName = "reservationservice")
@@ -58,10 +59,60 @@ public class ReservationService {
         reservationDao.deleteById(reservationId);
     }
 
+    @WebMethod
     public void updateSeats(Long reservationId, List<Long> seatIds) {
         Reservation reservation = reservationDao.findById(reservationId).orElseThrow(IllegalArgumentException::new);
         Set<Seat> seats = seatIds.stream().map(seatId -> seatDao.findById(seatId).orElse(null)).filter(Objects::nonNull).collect(Collectors.toSet());
         reservation.setSeats(seats);
         reservationDao.update(reservation);
+    }
+
+    @WebMethod
+    public void sendEmail(String mailTo) throws MessagingException, IOException {
+        Properties prop = getProperties();
+        Session session = getSession(prop);
+        Message message = createReservationMessage(session, mailTo);
+        Transport.send(message);
+    }
+
+    private Message createReservationMessage(Session session, String emailTo) throws MessagingException, IOException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("cinema.rsi.project@gmail.com"));
+        message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse(emailTo));
+        message.setSubject("Cinema-reservation");
+
+        String msg = "Thank you for your reservation.";
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+        // todo: creating PDF
+//        MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+//        attachmentBodyPart.attachFile(file);
+//        multipart.addBodyPart(attachmentBodyPart);
+        return message;
+    }
+
+    private Session getSession(Properties prop) {
+        return Session.getInstance(prop, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("cinema.rsi.project", "mati1234!");
+                }
+            });
+    }
+
+    private Properties getProperties() {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "25");
+        return prop;
     }
 }
